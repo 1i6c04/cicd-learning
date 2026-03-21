29ad5eb243aa4fe847d0afe34b179ded8aed4acd
1i6c04 <com45167@gmail.com>
2026-03-21 10:24:12 +0800

    feat: add assert false

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,4 +13,9 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function test_needs_to_be_failed(): void
+    {
+        $this->assertTrue(false);
+    }
 }
